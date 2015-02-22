0114481224b447fc58eea0e8dbbf5001d91e01df
Ankit Gupta <ag7bf@virginia.edu>
2015-02-22 18:35:55 -0500

    timer added

--- test2.go
+++ test2.go
@@ -69,6 +69,7 @@ func FindValidBlock(newTemplateChan chan bool, done chan bool, block *btcwire.Ms
 			done <- true
 			return
 		default:
+			timeStart := time.Now().Nanosecond()
 			for i := 0; i < hashesThenCheck; i++ {
 				blockSha, _ := block.Header.BlockSha()
 
@@ -87,6 +88,13 @@ func FindValidBlock(newTemplateChan chan bool, done chan bool, block *btcwire.Ms
 					}
 				}
 			}
+			timeFinish := time.Now().Nanosecond()
+			elapsedTime := timeFinish - timeStart
+
+			nanoSecondsPerHash := elapsedTime / hashesThenCheck
+
+			fmt.Println("nanoseconds per hash:", nanoSecondsPerHash)
+
 			loops += 1
 			fmt.Println(hashesThenCheck*loops, "hashes done. Checking for new template")
 		}
