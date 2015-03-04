f26749c02476c0d18847a8dac118aac3045335f1
Ankit Gupta <ag7bf@virginia.edu>
2015-03-03 20:43:39 -0500

    getting stuff out of json

--- txtracker/start.go
+++ txtracker/start.go
@@ -19,25 +19,49 @@ import (
 	"log"
 )
 
+
 func main() {
-	
+	print := fmt.Println
 	s := "\"Hello\""
 	fmt.Println(s)
 
 	o,_ := strconv.Unquote(s)
 	fmt.Println(o)
 
-
-
-
-
-
-
-
-
 	// command = pointctl getrawtransaction d2011b19dea6e98ec8bf78bd224856e76b6a9c460bbb347e49adb3dcf457e548
 	txid := "d2011b19dea6e98ec8bf78bd224856e76b6a9c460bbb347e49adb3dcf457e548"
-	cmd := exec.Command("pointctl", "getrawtransaction", txid)
+	txdetails := getTransactionDetails(txid)
+	txdetailsbytes := []byte(txdetails)
+
+
+	var f interface{}
+	_ = json.Unmarshal(txdetailsbytes, &f)
+	m := f.(map[string]interface{})
+
+	for k, v := range m {
+	    switch vv := v.(type) {
+	    case string:
+	        fmt.Println(k, "is string", vv)
+	    case int:
+	        fmt.Println(k, "is int", vv)
+	    case []interface{}:
+	        fmt.Println(k, "is an array:")
+	        for i, u := range vv {
+	            fmt.Println(i, u)
+	        }
+	    default:
+	        fmt.Println(k, "is of a type I don't know how to handle")
+	    }
+	}
+
+	txidreturned  := m["txid"]
+	print("\n\ngot txid", txidreturned)
+
+}
+
+func getTransactionDetails(txhash string) (string){
+	// command = pointctl getrawtransaction d2011b19dea6e98ec8bf78bd224856e76b6a9c460bbb347e49adb3dcf457e548
+	cmd := exec.Command("pointctl", "getrawtransaction", txhash)
 
 	var out bytes.Buffer
     cmd.Stdout = &out
@@ -46,7 +70,7 @@ func main() {
     	log.Fatal(err)
     }
     // fmt.Printf("result: %s\n", out)
-    fmt.Println(out.String())
+    // fmt.Println(out.String())
 
 
 	cmd2 := exec.Command("xargs", "pointctl", "decoderawtransaction")
@@ -58,29 +82,34 @@ func main() {
     	log.Fatal(err2)
     }
 	
-
-    fmt.Println(out2.String())
-
-
-	var f interface{}
-	err = json.Unmarshal(out2.Bytes(), &f)
-	m := f.(map[string]interface{})
-
-	for k, v := range m {
-    switch vv := v.(type) {
-    case string:
-        fmt.Println(k, "is string", vv)
-    case int:
-        fmt.Println(k, "is int", vv)
-    case []interface{}:
-        fmt.Println(k, "is an array:")
-        for i, u := range vv {
-            fmt.Println(i, u)
-        }
-    default:
-        fmt.Println(k, "is of a type I don't know how to handle")
-    }
+    return out2.String()
+    // fmt.Println(out2.String())
 }
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
