9f35bb55bc052b2803108c8583af6650a00444dd
Ankit Gupta <ag7bf@virginia.edu>
2015-03-04 00:58:23 -0500

    Printing input addresses and amounts of tx

--- txtracker/start.go
+++ txtracker/start.go
@@ -27,96 +27,48 @@ func main() {
 
 	o,_ := strconv.Unquote(s)
 	fmt.Println(o)
-
-	// command = pointctl getrawtransaction d2011b19dea6e98ec8bf78bd224856e76b6a9c460bbb347e49adb3dcf457e548
-	txid := "d2011b19dea6e98ec8bf78bd224856e76b6a9c460bbb347e49adb3dcf457e548"
+	// address: Prxy397nCyskwHwmiv3TaFG6ZgZ88Cbnju
+	// command = pointctl getrawtransaction c1de1be883834d733d096b3e14674978459f111f90d9dfbc5a82c9fa20db60a7
+	
+	txid := "c1de1be883834d733d096b3e14674978459f111f90d9dfbc5a82c9fa20db60a7"
 	txdetails := getTransactionDetails(txid)
-	txdetailsbytes := []byte(txdetails)
-
-
-	var f interface{}
-	_ = json.Unmarshal(txdetailsbytes, &f)
-	m := f.(map[string]interface{})
-
-
-	// for k, v := range m {
-	//     switch vv := v.(type) {
-	// 	    case string:
-	// 	        fmt.Println(k, "is string", vv)
-	// 	    case int:
-	// 	        fmt.Println(k, "is int", vv)
-	// 	    case []interface{}:
-	// 	        fmt.Println(k, "is an array:")
-	// 	        for i, u := range vv {
-	// 	            fmt.Println(i, u)
-	// 	        }
-	// 	    default:
-	// 	        fmt.Println(k, "is of a type I don't know how to handle")
-	// 	    }
-	// }
+	m := getTransactionJson(txdetails)
 
 	txidreturned  := m["txid"]
 	print("\n\ngot txid", txidreturned)
-
 
 	vinList := getVinList(m)
 	voutList := getVoutList(m)
 
 	_ , _ = vinList,voutList
-	// Given transaction json map, get list of vin's
-	// vinList := make([]vin,0)
-	// vinJsonList := m["vin"]
 
-	// switch vv := vinJsonList.(type) {
-	// case []interface{}:
-	// 	for _, u := range vv {
-	// 		j := u.(map[string]interface{})
-	// 		vinTxid := j["txid"].(string)
-	// 		vinVout := int(j["vout"].(float64))
-	// 		newVin := vin{txid: vinTxid, vout: vinVout}
-	// 		vinList = append(vinList, newVin)
- //            // fmt.Println(i, u)
- //        }
-	// 	// print("yes matches")
-	// default:
-	// 	print("nope didn't work")
-	// }
 
-	// print("vins:")
-	// for _,x := range vinList {
-	// 	print(x)
-	// }
 
-	// Given transaction json map, get list of vout's
+	// Start with transaction
 
-	// voutList := make([]vout,0)
-	// voutJsonList := m["vout"]
+	// See input addresses of transaction as well as amounts
+	// For each vin, going to have to 
+	fmt.Println("Outputs")
+	for i, x := range vinList {
+		tx := getTransactionDetails(x.txid)
+		txjs := getTransactionJson(tx)
+		txvouts := getVoutList(txjs)
+		for _, y := range txvouts {
 
-	// switch oo := voutJsonList.(type) {
-	// case []interface{}:
-	// 	for _,u := range oo {
-	// 		j := u.(map[string]interface{})
-	// 		voutVal := j["value"].(float64)
-	// 		voutN := int(j["n"].(float64))
+			if y.n == x.vout {
+				fmt.Println("\t[",i,"]",y.addresses[0],y.value)
+				break
+			}
+		}
+	}
+		// 1) Get tx, 
+		// 2) Get 'n'th output
+		// 3) Get address and amount of that output
 
-	// 		vScriptPubKey := j["scriptPubKey"].(map[string]interface{})
-	// 		vAddresses := vScriptPubKey["addresses"].([]interface{})
-	// 		vAddressesStrings := make([]string, 0)
-	// 		for _,u := range vAddresses {
-	// 			addr := u.(string)
-	// 			vAddressesStrings = append(vAddressesStrings, addr)
-	// 		}
 
-	// 		newVout := vout{value: voutVal, n: voutN, addresses: vAddressesStrings}
-	// 		voutList = append(voutList, newVout)
-	// 	}
-	// }
-
-	// print("vouts:")
-	// for _,x := range voutList {
-	// 	print(x)
-	// }
-
+	// See output addresses as well as amounts
+	// For each vout
+		// 1)Print address and amount
 
 
 
@@ -190,6 +142,15 @@ func getVoutList(m map[string] interface{}) ([]vout) {
 		fmt.Println(x)
 	}
 	return voutList
+}
+
+func getTransactionJson(txdetails string) (map[string]interface{}){
+	txdetailsbytes := []byte(txdetails)
+
+	var f interface{}
+	_ = json.Unmarshal(txdetailsbytes, &f)
+	m := f.(map[string]interface{})
+	return m
 }
 
 func getTransactionDetails(txhash string) (string){
